34df7b012ece4843ba18a6c3015f26bb12bf2fab
Tirth Patel <tirthpatel49@gmail.com>
2016-11-12 13:13:09 +0530

    Fix path

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -6,3 +6,7 @@ ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 
 # include the non-open-source counterpart to this file
 -include vendor/Micromax/A311/AndroidBoardVendor.mk
+
+# copy keylayout overrides
+$(shell mkdir -p $(TARGET_OUT_KEYLAYOUT); \
+    cp -pf device/Micromax/A311/keylayout/Generic.kl $(TARGET_OUT_KEYLAYOUT))
