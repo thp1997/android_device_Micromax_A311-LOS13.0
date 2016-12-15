faa2c09516fb2b5e4ed2ca036a0dea4d3d8e35fc
Tirth Patel <tirthpatel49@gmail.com>
2016-12-15 18:24:55 +0530

    Unnecessary Permission.

--- device_A311.mk
+++ device_A311.mk
@@ -109,7 +109,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml
-    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
 
 # Keylayout overrides
 PRODUCT_COPY_FILES_OVERRIDES += \
